198e30a3cb9515e3213bd81e726bd31b17ebeffd
Nazarii Lysak <nazarlv87@gmail.com>
2021-05-29 14:21:57 +0300

    ready

--- js/index.js
+++ js/index.js
@@ -8,21 +8,3 @@ burgerMenu.addEventListener('click', function () {
     })
 });
 
-// ------------------------------------------------
-
-
-document.body.onclick = function (event) {
-    event = event || window.event;
-        
-    if (event.target.classList.contains('goods-min')) {
-        let allDiv = document.querySelectorAll('.goods-img-min div');
-        for (var i = 0; i < allDiv.length; i++) {
-            allDiv[i].classList.remove('active');
-        }
-        document.getElementById('goods-max').src = event.target.src;
-        console.log(event.target.parentNode);
-        let parentDiv = event.target.parentElement;
-        parentDiv.classList.add('active');
-    } 
-};
-
